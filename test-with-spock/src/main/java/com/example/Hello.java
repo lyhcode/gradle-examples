930f7534ab5fc11cb528dee872e766d430f09e25
lyhcode (Yan-hong Lin) <lyhcode@gmail.com>
2011-11-02 19:01:58 +0800

    updated

--- test-with-spock/src/main/java/com/example/Hello.java
+++ test-with-spock/src/main/java/com/example/Hello.java
@@ -2,6 +2,8 @@ package com.example;
 
 public class Hello {
 	public String sayHello(String who) {
-		return "Hello, " + who;
+		System.out.println("Say hello to " + who);
+		System.err.println("Error: Say hello to " + who);
+		return "Hello " + who;
 	}
 }
