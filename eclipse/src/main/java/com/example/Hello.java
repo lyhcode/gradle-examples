0afde26f5ff44e8d79ae7c3f2c0742dd3a12a0a6
lyhcode (Yan-hong Lin) <lyhcode@gmail.com>
2011-12-27 18:16:42 +0800

    add eclipse examples

---
+++ eclipse/src/main/java/com/example/Hello.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Hello {
+	public static void main(String args[]) {
+		System.out.println("Hello World!");
+		System.out.println(StringUtils.isBlank("test"));
+	}
+}
